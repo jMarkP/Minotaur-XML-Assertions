1d63fd785b73166a558e9b4af012ae017aba1fb1
J. Mark Pim <j.mark.pim+windows@gmail.com>
2011-08-13 16:30:45 +0100

    First passing step definitions

--- specs/MinotaurSpecs/ComparingXml.feature
+++ specs/MinotaurSpecs/ComparingXml.feature
@@ -4,15 +4,15 @@
 	I want to be able to test if two xml documents are equivalent
 
 Scenario: Comparing matching XDocuments
-	Given I have a reference XDocument with elements:
+	Given I have a reference XDocument with root "root" and elements:
 		| element   | id    | value      |
 		| item      | 1     | Item one   |
 		| item      | 2     | Item two   |
 		| summary   | null  | Two items  |
-	And I have a XDocument under test with elements:
+	And I have a test XDocument with root "root" and elements:
 		| element   | id    | value      |
 		| item      | 1     | Item one   |
 		| item      | 2     | Item two   |
 		| summary   | null  | Two items  |
-	When I assert the XDocument under test should be equivalent to the reference XDocument
+	When I assert the test XDocument should be equivalent to the reference XDocument
 	Then no exception should be thrown
---
+++ specs/MinotaurSpecs/StepDefinitions/ComparingXmlSteps.cs
@@ -0,0 +1,59 @@
+﻿using System.Xml.Linq;
+using TechTalk.SpecFlow;
+
+namespace MinotaurSpecs.StepDefinitions
+{
+    [Binding]
+    public class ComparingXmlSteps
+    {
+#region Test fields
+        private readonly XDocument referenceDocument = new XDocument();
+        private readonly XDocument testDocument = new XDocument();
+#endregion
+
+        [StepArgumentTransformation]
+        public XDocument NamedXDocumentTransform(string xdocumentName)
+        {
+            switch (xdocumentName)
+            {
+                case "reference":
+                    return referenceDocument;
+                case "test":
+                    return testDocument;
+            }
+
+            return null;
+        }
+
+        [Given(@"I have a (.*) XDocument with root ""(.*)"" and elements:")]
+        public void GivenIHaveAReferenceXDocumentWithElements(XDocument document, string rootElement, Table table)
+        {
+            var root = new XElement(rootElement);
+
+            foreach (var row in table.Rows)
+            {
+                var newItem = 
+                    new XElement(row["element"],
+                        new XAttribute("id", row["id"]),
+                        new XText(row["value"]));
+
+                root.Add(newItem);
+            }
+
+            document.Add(root);
+        }
+
+        [When(@"I assert the XDocument under test should be equivalent to the reference XDocument")]
+        public void WhenIAssertTheXDocumentUnderTestShouldBeEquivalentToTheReferenceXDocument()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"no exception should be thrown")]
+        public void ThenNoExceptionShouldBeThrown()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
